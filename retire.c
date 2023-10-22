fe75902c2e5aedac89029b95160daf6c95385ac3
MUHAMMADAZAN1 <144342317+MUHAMMADAZAN1@users.noreply.github.com>
2023-10-22 20:44:35 +0500

    variables made as per programe

---
+++ retire.c
@@ -0,0 +1,8 @@
+//The prgram about 401(k) plan is a account for people to save money for retirement
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+int main(){
+    //made variable according the program computation
+    float initialAmount, monthlyContributionAmount, annualReturn, annualInflation, balance;
+    float interest, adjustInflationReturnRate, totalInterestEarned=0;
